890bdbcd5e92296b838c6b0e5d286bb5b8731fa7
MT1EL <tokochixladze3@gmail.com>
2025-09-16 14:18:04 +0400

    fix: icons custom stroke width

--- components/icons/MarkerPinIcon.tsx
+++ components/icons/MarkerPinIcon.tsx
@@ -18,7 +18,7 @@ function MarkerPinIcon({ width, height, stroke, ...props }: IconProps) {
       <Path
         d='M4.167 11.905c-1.543.68-2.5 1.63-2.5 2.678 0 2.071 3.731 3.75 8.333 3.75 4.603 0 8.334-1.679 8.334-3.75 0-1.049-.957-1.997-2.5-2.678M15 6.667c0 3.386-3.75 5-5 7.5-1.25-2.5-5-4.114-5-7.5a5 5 0 1110 0zm-4.166 0a.833.833 0 11-1.667 0 .833.833 0 011.667 0z'
         stroke={stroke ?? props.color ?? Colors.icon.primary}
-        strokeWidth={1.39167}
+        strokeWidth={props.strokeWidth ?? 1.39167}
         strokeLinecap='round'
         strokeLinejoin='round'
       />
@@ -26,4 +26,5 @@ function MarkerPinIcon({ width, height, stroke, ...props }: IconProps) {
   );
 }
 
+MarkerPinIcon.displayName = 'MarkerPinIcon';
 export default React.memo(MarkerPinIcon);
--- components/icons/PieChartIcon.tsx
+++ components/icons/PieChartIcon.tsx
@@ -18,7 +18,7 @@ function PieChartIcon({ width, height, stroke, ...props }: IconProps) {
       <Path
         d='M10 1.667A8.334 8.334 0 0118.334 10M10 1.667V10m0-8.333A8.333 8.333 0 1018.334 10M10 1.667A8.333 8.333 0 0118.334 10m0 0H10m8.334 0a8.333 8.333 0 01-3.435 6.742L10 10'
         stroke={stroke ?? props.color ?? Colors.icon.primary}
-        strokeWidth={1.39167}
+        strokeWidth={props.strokeWidth ?? 1.39167}
         strokeLinecap='round'
         strokeLinejoin='round'
       />
